9cf5543094b7296016ad134ab48a44189dd1f5de
musicapple <jaker0109@naver.com>
2023-06-27 19:39:27 +0900

    commit test

--- fProject/src/main/java/com/project/fin/store/controller/StoreController.java
+++ fProject/src/main/java/com/project/fin/store/controller/StoreController.java
@@ -1,5 +1,7 @@
 package com.project.fin.store.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.fin.store.model.service.StoreService;
+import com.project.fin.store.model.vo.Store;
 
 @Controller
 @RequestMapping("/store")
@@ -16,10 +19,28 @@ public class StoreController {
 	@Autowired
 	private StoreService storeservice;
 	
+	/*
+	 * @GetMapping("/storeList.me") public String storeList(int category, Model
+	 * model) { model.addAttribute("categoryNo",category); return
+	 * "redirect:/store/storeList"; }
+	 */
+	@GetMapping("/storeList.me")
+	public String storeList(int category, Model model) {
+		
+		List<Store> storeList = storeservice.selectStoreList();
+		
+		model.addAttribute("category", category);
+		model.addAttribute("storeList",storeList);
+		
+		return "store/storeList";
+	}
 	
-	  @GetMapping("/storeList.me") public String storeList(int category, Model model) { 
-		  model.addAttribute("categoryNo",category); 
-		  return "redirect:/store/storeList"; 
-		  }
+	@GetMapping("/storeDetail.me")
+	public String storeDetail(int storeNo, Model model) {
+		
+		Store store = storeservice.selectOneStore(storeNo);
+		model.addAttribute("store", store);
+		return "store/storeDetail";
+	}
 	
 }
--- fProject/src/main/java/com/project/fin/store/model/service/StoreService.java
+++ fProject/src/main/java/com/project/fin/store/model/service/StoreService.java
@@ -1,5 +1,13 @@
 package com.project.fin.store.model.service;
 
+import java.util.List;
+
+import com.project.fin.store.model.vo.Store;
+
 public interface StoreService {
+	
+	List<Store> selectStoreList();
+
+	Store selectOneStore(int storeNo);
 
 }
